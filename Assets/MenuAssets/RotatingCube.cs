b5eb3b9b41a7039fe84576f0b29c944aafa7e80f
sagaw <eta19777@gmail.com>
2024-04-11 18:22:07 +0300

    add

--- Assets/MenuAssets/RotatingCube.cs
+++ Assets/MenuAssets/RotatingCube.cs
@@ -13,6 +13,6 @@ public class RotatingCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up*0.2f);
+        transform.Rotate(0,0,0.4f);
     }
 }
